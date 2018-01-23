152194a859ba55f6d323edcdcaefbffde0e1a440
Kevin Mikus <kevinpmikus@gmail.com>
2018-01-23 15:14:51 -0500

    semi-functioning placeholder

---
+++ Team 2 IST440 Game/Assets/Scripts/PlayerRun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRun : MonoBehaviour {
+
+    private Rigidbody2D playerBody2d;
+    private float speed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+        playerBody2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey) { this.transform.Translate(0.1f*Input.GetAxis("Horizontal"), 0, 0); }
+    }
+}
