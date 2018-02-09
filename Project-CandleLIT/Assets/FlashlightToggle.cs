abf909d09740c860664f0790d79ea97cbda5ae6a
kmikus <kevinpmikus@gmail.com>
2018-02-09 11:19:26 -0500

    added script to toggle the flashlight with e

---
+++ Project-CandleLIT/Assets/FlashlightToggle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightToggle : MonoBehaviour {
+
+    private Light flashlightLight;
+    private bool lightOn = false;
+
+	// Use this for initialization
+	void Start () {
+        flashlightLight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lightOn)
+        {
+            flashlightLight.intensity = 40;
+        } else
+        {
+            flashlightLight.intensity = 0;
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            lightOn = !lightOn;
+        }
+	}
+}
