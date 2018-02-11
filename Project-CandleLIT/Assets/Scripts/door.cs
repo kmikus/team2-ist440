fff704a594fd6c7c7bcdc64a56e9a468027cbb64
KateCoppola <klc5730@psu.edu>
2018-02-11 14:34:48 -0500

    there is now a script that will allow the player to go to the next level once they get to the end of the level. put a placeholder door in there for now.

---
+++ Project-CandleLIT/Assets/Scripts/door.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class door : MonoBehaviour {
+
+    public int LevelToLoad;
+    private GameMaster gm;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gm.DoorText.text = ("[e] to Enter");
+            if (Input.GetKeyDown("e"))
+            {
+                SceneManager.LoadScene(LevelToLoad);
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                SceneManager.LoadScene(LevelToLoad);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gm.DoorText.text = ("");
+        }
+            
+    }
+}
