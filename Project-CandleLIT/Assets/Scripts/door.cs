81d43becdbfa16ad5d4488efa478895776aa9bf1
KateCoppola <klc5730@psu.edu>
2018-02-23 16:55:12 -0500

    When player collides with door, they are now prompted to enter it. when they enter it, their position is transformed. for now they are just transromed to the beginning of the level, but obviously they will be transformed to another level usually

--- Project-CandleLIT/Assets/Scripts/door.cs
+++ Project-CandleLIT/Assets/Scripts/door.cs
@@ -1,49 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+public class Door : MonoBehaviour {
 
-public class door : MonoBehaviour {
-
-    public int LevelToLoad;
-    private GameMaster gm;
-
-    void Start()
-    {
-        gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
-    }
-
+  
+    public Text DoorText;
+    public Vector3 tempPos;
+    
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Door"))
         {
-            gm.DoorText.text = ("[d] to Enter");
-            if (Input.GetKeyDown("d"))
+            DoorText.text = ("[t] to enter");
+
+            if (Input.GetKeyDown("t"))
             {
-                SceneManager.LoadScene(LevelToLoad);
+                
+                tempPos = transform.position;
+                tempPos = new Vector3(-39.7f, 13.1f, 0f);
+                transform.position = tempPos;
+
             }
         }
     }
 
     void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Door"))
         {
-            if (Input.GetKeyDown("d"))
+            if (Input.GetKeyDown("t"))
             {
-                SceneManager.LoadScene(LevelToLoad);
+                tempPos = transform.position;
+                tempPos = new Vector3(-39.7f, 13.1f, 0f);
+                transform.position = tempPos;
             }
-        }
     }
+        }
 
     void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            gm.DoorText.text = ("");
-        }
-            
+        DoorText.text = ("");
     }
 }
