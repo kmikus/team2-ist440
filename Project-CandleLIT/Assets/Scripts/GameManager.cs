c293d9df2db865e95504e76c1ef01530ac49c022
kmikus <kevinpmikus@gmail.com>
2018-02-25 22:46:31 -0500

    added joe art

---
+++ Project-CandleLIT/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GameManager: MonoBehaviour
+{
+    private static bool running = false;
+    private static int lives = 2;
+    public static bool Running
+    {
+        get { return running; }
+        set
+        {
+            running = value;
+        }
+    }
+    public static int Lives
+    {
+        get { return lives; }
+        set
+        {
+            lives = value;
+        }
+    }
+    public static void EndGame(GameObject gameOverText)
+    {
+        running = false;
+        gameOverText.SetActive(true);
+    }
+}
