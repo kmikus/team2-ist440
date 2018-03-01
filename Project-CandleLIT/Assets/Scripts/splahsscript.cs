0311276081690d633de221aef5ec6a2576a7f5af
jibsm_000 <jibsma1016@gmail.com>
2018-03-01 17:18:01 -0500

    splash screen animation

---
+++ Project-CandleLIT/Assets/Scripts/splahsscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class splahsscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(LoadMainMenu());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator LoadMainMenu()
+    {
+        yield return new WaitForSeconds(3f);
+
+        SceneManager.LoadScene("Menu_Main");
+    }
+}
