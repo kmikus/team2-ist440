2cc8e6d89c68e0143b7a9c5561d5b529d7ccb5fc
KateCoppola <klc5730@psu.edu>
2018-03-07 23:49:54 -0500

    some minor fixes and also started working on script for picking up key and using it to unlock door

---
+++ Project-CandleLIT/Assets/Scripts/Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    public GameObject[] inventory = new GameObject[10];
+
+    public void AddItem(GameObject Key)
+    {
+        bool itemAdded = false;
+
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory[i] == null)
+            {
+                inventory[i] = Key;
+                itemAdded = true;
+                break;
+            }
+        }
+    }
+}
---
+++ Project-CandleLIT/Assets/Scripts/KeyPickUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPickUp : MonoBehaviour {
+
+    public GameObject currentKey = null;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+
+        {
+            currentKey = collision.gameObject;
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (collision.gameObject == currentKey)
+
+            {
+                currentKey = null;
+            }
+        }
+    }
+}
