bad300d486a0e6368c43a723c6cf4e644b713ca5
JITHIN KOSHY <jithinkoshy@JITHINs-MacBook-Pro.local>
2018-03-11 14:04:46 -0400

    Footsteps audio fix

--- Project-CandleLIT/Assets/Scripts/FootstepsAudio.cs
+++ Project-CandleLIT/Assets/Scripts/FootstepsAudio.cs
@@ -1,34 +1,43 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FootstepsAudio : MonoBehaviour {
-
-
-	// Use this for initialization
-	void Start () {
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-		PlayStopFootSteps ();
-
-	}
-
-	void PlayStopFootSteps()
-	{
-		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.RightArrow)) {
-
-			if (!GetComponent<AudioSource> ().isPlaying)
-				GetComponent<AudioSource> ().Play ();
-		} 
-		else 
-		{
-			if (GetComponent<AudioSource> ().isPlaying)
-				GetComponent<AudioSource> ().Stop();
-		}
-
-	}
+    
+    public bool grounded;
+    public LayerMask ground;
+    
+    Collider2D playerCollider;
+    
+    // Use this for initialization
+    void Start () {
+        
+        playerCollider = GetComponent<Collider2D> ();
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+        PlayStopFootSteps ();
+        
+    }
+    
+    void PlayStopFootSteps()
+    {
+        grounded = playerCollider.IsTouchingLayers (ground);
+        
+        if ((Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.RightArrow)) && grounded) {
+            
+            if (!GetComponent<AudioSource> ().isPlaying)
+            GetComponent<AudioSource> ().Play ();
+        }
+        else
+        {
+            if (GetComponent<AudioSource> ().isPlaying)
+            GetComponent<AudioSource> ().Stop();
+        }
+        
+    }
 }
+
