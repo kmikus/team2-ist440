75e0ac646b95ae0e8ce0e0374a699b604549d5a7
Mibin Baby <Mibin@Mibins-MacBook-Pro.local>
2018-03-12 19:08:43 -0400

    Added new enemies

---
+++ Project-CandleLIT/Assets/Scripts/BlueEnemyScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueEnemyScript : MonoBehaviour {
+
+	public float velocity = 1f;
+
+	public Transform sightStart;
+	public Transform sightEnd;
+
+
+	public bool colliding;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (velocity, GetComponent<Rigidbody2D>().velocity.y);
+
+		colliding = Physics2D.Linecast (sightStart.position, sightEnd.position);
+
+		if (colliding) {
+
+			transform.localScale = new Vector2 (transform.localScale.x * -1, transform.localScale.y);
+			velocity*= -1;
+
+		}
+
+	}
+
+}
