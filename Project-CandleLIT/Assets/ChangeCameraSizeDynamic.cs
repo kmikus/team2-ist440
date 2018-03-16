ed24884b6bcf8bfce93a172c56b4f909b8bf4284
kmikus <kevinpmikus@gmail.com>
2018-03-16 13:25:14 -0400

    Added a basic scaling camera (needs to have a smoother effect and scaling back down). Also did some housekeeping in the folders. Tried to fix the black line problem with the tiles but no dice.

---
+++ Project-CandleLIT/Assets/ChangeCameraSizeDynamic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCameraSizeDynamic : MonoBehaviour {
+
+    public Transform player1;
+    public Transform player2;
+    public float shiftThreshold = 0.7f;
+    public float sizeIncrease = 1.25f;
+
+    private Camera cam;
+    private float camHeight;
+    private float camWidth;
+
+    private float playerDistanceX;
+    private float playerDistanceY;
+
+	// Use this for initialization
+	void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        camHeight = cam.orthographicSize * 2;
+        camWidth = cam.orthographicSize * 2 * cam.aspect;
+
+        Debug.Log("Height: " + camHeight);
+        Debug.Log("Width: " + camWidth);
+
+        playerDistanceX = Mathf.Abs(player1.position.x - player2.position.x);
+        playerDistanceY = Mathf.Abs(player1.position.y - player2.position.y);
+
+        Debug.Log("Player X dist: " + playerDistanceX);
+        Debug.Log("Player Y dist: " + playerDistanceY);
+
+        if (playerDistanceX > shiftThreshold * camWidth)
+        {
+            cam.orthographicSize *= sizeIncrease;
+        } else if (playerDistanceY > shiftThreshold * camHeight)
+        {
+            cam.orthographicSize *= sizeIncrease;
+        }
+
+    }
+}
---
+++ Project-CandleLIT/Assets/Scripts/Camera/UpdatePosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatePosition : MonoBehaviour {
+
+    public Transform player1;
+    public Transform player2;
+
+    private Transform currentPosition;
+    private float newX;
+    private float newY;
+
+	// Use this for initialization
+	void Start () {
+        currentPosition = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        newX = (player1.position.x + player2.position.x) / 2;
+        newY = (player1.position.y + player2.position.y) / 2;
+
+        currentPosition.position = new Vector3(newX, newY, currentPosition.position.z);
+    }
+}
