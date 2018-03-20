261bf71d9bb97ecd8ea493f39330ec35b50fbab8
kmikus <kevinpmikus@gmail.com>
2018-03-20 16:48:17 -0400

    fixed the problem with the enemies not respawning correctly and also fixed the particle effects not being destroyed

--- Project-CandleLIT/Assets/Scripts/Player/FlaslightDamageEnemy.cs
+++ Project-CandleLIT/Assets/Scripts/Player/FlaslightDamageEnemy.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class FlaslightDamageEnemy : MonoBehaviour {
 
-    private GameObject enemy;
+    private List<GameObject> enemies = new List<GameObject>();
+    private int enemyIndex=0;
     private Flashlight fl;
-    private float t = 0f;
+    private List<float> t = new List<float>();
 
     private float particleTimer = 0f;
     private bool particleAdded = false;
@@ -22,17 +23,24 @@ public class FlaslightDamageEnemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (enemy != null && !enemy.activeSelf)
+        if (enemies.Count > 0)
         {
-            t += Time.deltaTime;
-        }
+            for (int i=0; i < enemies.Count; i++)
+            {
+                if (enemies[i] != null && !enemies[i].activeSelf)
+                {
+                    t[i] += Time.deltaTime;
+                }
 
-        if (t > disableTime)
-        {
-            enemyPosition = enemy.GetComponent<Transform>();
-            Instantiate(respawnParticle, new Vector3(enemyPosition.position.x, enemyPosition.position.y + 1, enemyPosition.position.z), Quaternion.identity);
-            enemy.SetActive(true);
-            t = 0;
+                if (t[i] > disableTime)
+                {
+                    enemyPosition = enemies[i].GetComponent<Transform>();
+                    var rp = Instantiate(respawnParticle, new Vector3(enemyPosition.position.x, enemyPosition.position.y + 1, enemyPosition.position.z), Quaternion.identity);
+                    enemies[i].SetActive(true);
+                    Destroy(rp, 2f);
+                    t[i] = 0;
+                }
+            }
         }
 
 	}
@@ -41,10 +49,13 @@ public class FlaslightDamageEnemy : MonoBehaviour {
     {
         if (collider.gameObject.tag == "Enemy" && fl.LightOn)
         {
-            enemy = collider.gameObject;
+            var enemy = collider.gameObject;
+            enemies.Add(enemy);
+            t.Add(0);
             enemyPosition = enemy.GetComponent<Transform>();
-            var particle = Instantiate(deathParticle, new Vector3(enemyPosition.position.x, enemyPosition.position.y, enemyPosition.position.z), Quaternion.identity);
+            var dp = Instantiate(deathParticle, new Vector3(enemyPosition.position.x, enemyPosition.position.y, enemyPosition.position.z), Quaternion.identity);
             enemy.SetActive(false);
+            Destroy(dp, 2f);
         }
     }
 }
