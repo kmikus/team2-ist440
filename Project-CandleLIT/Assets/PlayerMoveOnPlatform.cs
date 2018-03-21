566bffa981427e6e3ca33a486810cfcc7ef7735e
KateCoppola <klc5730@psu.edu>
2018-03-21 16:41:46 -0400

    player can stand on platform and move with it

---
+++ Project-CandleLIT/Assets/PlayerMoveOnPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveOnPlatform : MonoBehaviour
+{
+    private GameObject platform = null;
+    private Vector3 offset;
+
+    void Start()
+{
+        platform = null;
+}
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        platform = col.gameObject;
+        offset = platform.transform.position - transform.position;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        platform = null;
+    }
+
+    void LateUpdate()
+    {
+       if (platform != null)
+        {
+            platform.transform.position = transform.position + offset;
+        } 
+    }
+}
