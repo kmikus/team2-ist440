46a1b742b845e36fe3c8ca73120ff4738ec1bcb7
KateCoppola <klc5730@psu.edu>
2018-03-22 18:47:45 -0400

    there is now cooperative elements where a button can be pressed to open the door for another player. its currently randomly placed in level 2 but it will be used in level 3 and so on.

---
+++ Project-CandleLIT/Assets/StepButtonSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepButtonSwitch : MonoBehaviour {
+
+    public BarrierDoor door;
+    public bool isOpen;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            door.DoorOpens();
+            isOpen = true;
+        }
+        else
+        {
+            door.DoorCloses();
+            isOpen = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        door.DoorCloses();
+        isOpen = false;
+    }
+
+
+}
