6b2094faabe4c3af1f43ec88aa96c25d9fda9c36
Kevin Mikus <kevinpmikus@gmail.com>
2018-03-26 14:53:16 -0400

    added start location script for 2 player mode

---
+++ Project-CandleLIT/Assets/Scripts/StartPlayerLocation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPlayerLocation : MonoBehaviour {
+
+    private Transform[] startTransforms;
+	// Use this for initialization
+	void Start () {
+        startTransforms = GetComponentsInChildren<Transform>();
+
+        foreach (var startTransform in startTransforms)
+        {
+            if (startTransform.gameObject.name == "Player1Start")
+            {
+                var p1 = GameObject.Find("Player1");
+                p1.transform.position = startTransform.position;
+            } else if (startTransform.gameObject.name == "Player2Start")
+            {
+                var p2 = GameObject.Find("Player2");
+                p2.transform.position = startTransform.position;
+            }
+        }
+	}
+}
