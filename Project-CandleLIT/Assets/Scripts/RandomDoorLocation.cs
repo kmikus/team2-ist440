e46a1f3705022239deb803897647a7734dbf6691
Kevin Mikus <kevinpmikus@gmail.com>
2018-03-26 15:15:50 -0400

    random door locations working

---
+++ Project-CandleLIT/Assets/Scripts/RandomDoorLocation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDoorLocation : MonoBehaviour {
+
+    public GameObject door;
+
+    private Transform[] doorLocations;
+
+	// Use this for initialization
+	void Start () {
+        doorLocations = GetComponentsInChildren<Transform>();
+
+        if (doorLocations.Length > 1)
+        {
+            door.transform.position = doorLocations[Random.Range(1, doorLocations.Length)].position;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
