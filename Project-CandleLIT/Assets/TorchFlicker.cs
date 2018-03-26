4ab7c5d511d26b01651ab9b950e16c8f93c5089e
kmikus <kevinpmikus@gmail.com>
2018-03-25 23:00:50 -0400

    added torch lighting effect

---
+++ Project-CandleLIT/Assets/TorchFlicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchFlicker : MonoBehaviour {
+
+    private Light torchLight;
+
+    public float baseRange = 8f;
+    public float rangeFactor = 5f;
+
+    public float baseAngle = 90f;
+    public float angleFactor = 20f;
+
+    public float speed = 5f;
+	// Use this for initialization
+	void Start () {
+        torchLight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        torchLight.range = baseRange + (Mathf.Abs(Mathf.Sin(Time.time * speed)) * rangeFactor);
+        torchLight.spotAngle = baseAngle - (Mathf.Abs(Mathf.Sin(Time.time * speed)) * angleFactor);
+
+    }
+}
