46d50afa58fd76813bc9b3eaa3168e76a35fa440
Kevin Mikus <kevinpmikus@gmail.com>
2018-03-27 12:26:29 -0400

    partially fixed menu input problem

--- Project-CandleLIT/Assets/selectOnInput.cs
+++ Project-CandleLIT/Assets/selectOnInput.cs
@@ -9,22 +9,9 @@ public class selectOnInput : MonoBehaviour {
     public GameObject selectedObject;
 
     private bool buttonSelected;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetAxisRaw ("Vertical") != 0 && buttonSelected == false)
-        {
-            eventSystem.SetSelectedGameObject(selectedObject);
-            buttonSelected = true;
-        }
-	}
 
-    private void OnDisable()
+    private void OnEnable()
     {
-        buttonSelected = false;
+        eventSystem.SetSelectedGameObject(selectedObject);
     }
 }
