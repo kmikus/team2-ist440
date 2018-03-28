2c9ee77a824104aba80388118cf72195520d90c6
jibsm_000 <jibsma1016@gmail.com>
2018-03-28 00:17:45 -0400

    pause menu doesnt overlap but joystick control still bad, better looking credit menu, added one player button to main menu

--- Project-CandleLIT/Assets/Scripts/PauseMenu.cs
+++ Project-CandleLIT/Assets/Scripts/PauseMenu.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour {
 
     public static bool GameIsPaused = false;
-    public GameObject pauseMenuUI;
+    public GameObject mainPanel;
+    //public GameObject pauseMenuUI;
+    public GameObject controlpanel;
+    public GameObject volume;
+    //public float timeScale = 1f;
+
     // Use this for initialization
     void Start()
     {
@@ -15,36 +20,56 @@ public class PauseMenu : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+
+        if (Input.GetKeyDown(KeyCode.Escape) && mainPanel.active == false)
         {
-            if (GameIsPaused)
-            {
-                Resume();
-            }else
-            {
-                Pause();
-            }
+            mainPanel.SetActive(true);
+            controlpanel.SetActive(false);
+            volume.SetActive(false);
+            Time.timeScale = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && mainPanel.active == true)
+        {
+            Time.timeScale = 1;
+            mainPanel.SetActive(false);
+            controlpanel.SetActive(false);
+            volume.SetActive(false);
         }
     }
 
     public void Resume()
     {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
+      /*  pauseMenuUI.SetActive(false);
         GameIsPaused = false;
+        Time.timeScale = timeScale;
+        */
+        mainPanel.SetActive(false);
+        controlpanel.SetActive(false);
+        volume.SetActive(false);
+        Time.timeScale = 1;
+
     }
 
-    public void Pause()
+   /* public void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
-    }
+    }*/
 
     public void loadMenu()
     {
         Application.LoadLevel("MainMenu");
         Time.timeScale = 1f;
         GameIsPaused = false;
+    }
+
+    public void Control()
+    {
+
+        mainPanel.SetActive(false);
+        controlpanel.SetActive(true);
+        volume.SetActive(false);
+
     }
 }
