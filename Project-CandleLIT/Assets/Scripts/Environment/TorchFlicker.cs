78719c94c27e0008dfee6ac8d40f638e0dc306c2
Kevin Mikus <kevinpmikus@gmail.com>
2018-04-08 10:36:41 -0400

    torches now have a random offset to make them not syncronous

--- Project-CandleLIT/Assets/Scripts/Environment/TorchFlicker.cs
+++ Project-CandleLIT/Assets/Scripts/Environment/TorchFlicker.cs
@@ -13,15 +13,19 @@ public class TorchFlicker : MonoBehaviour {
     public float angleFactor = 20f;
 
     public float speed = 5f;
+
+    private float randomOffset;
+
 	// Use this for initialization
 	void Start () {
         torchLight = GetComponent<Light>();
+        randomOffset = Random.Range(0, 10);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        torchLight.range = baseRange + (Mathf.Abs(Mathf.Sin(Time.time * speed)) * rangeFactor);
-        torchLight.spotAngle = baseAngle - (Mathf.Abs(Mathf.Sin(Time.time * speed)) * angleFactor);
+        torchLight.range = baseRange + (Mathf.Abs(Mathf.Sin(Time.time * speed + randomOffset)) * rangeFactor);
+        torchLight.spotAngle = baseAngle - (Mathf.Abs(Mathf.Sin(Time.time * speed + randomOffset)) * angleFactor);
 
     }
 }
