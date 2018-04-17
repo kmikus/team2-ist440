d6780a44b289a583bf9c3442ae65934efb3f7151
jibsm_000 <jibsma1016@gmail.com>
2018-04-17 17:38:54 -0400

    added 321 countdown to vm

---
+++ Project-CandleLIT/Assets/delaystart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delaystart : MonoBehaviour {
+
+    public GameObject countdown;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("StartDelay");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator StartDelay()
+    {
+        Time.timeScale = 0;
+        float pauseTime = Time.realtimeSinceStartup + 3f;
+        while (Time.realtimeSinceStartup < pauseTime)
+            yield return 0;
+        countdown.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
