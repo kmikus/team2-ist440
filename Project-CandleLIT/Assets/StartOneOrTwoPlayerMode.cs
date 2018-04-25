b82071d087f281f2890c170ef956ca5ef3eb633c
kmikus <kevinpmikus@gmail.com>
2018-04-25 13:31:38 -0400

    added buttons for selecting 1 and 2 player mode. They are currently 1 and 2 on the keyboard with Inputs remaining to be mapped for the arcade

---
+++ Project-CandleLIT/Assets/StartOneOrTwoPlayerMode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartOneOrTwoPlayerMode : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Start1PlayerKeyboard") || Input.GetButtonDown("Start1PlayerArcade"))
+        {
+            NumPlayersChecker.setNumPlayers(1);
+            SceneManager.LoadScene("Level One V2");
+        }
+        else if (Input.GetButtonDown("Start2PlayerKeyboard") || Input.GetButtonDown("Start2PlayerArcade"))
+        {
+            NumPlayersChecker.setNumPlayers(2);
+            SceneManager.LoadScene("Level One V2");
+        }
+    }
+}
