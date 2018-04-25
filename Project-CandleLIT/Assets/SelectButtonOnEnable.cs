d485be7b3b3b71fda708a837d98030e5ee386294
kmikus <kevinpmikus@gmail.com>
2018-04-25 13:12:20 -0400

    fixed selecting issue for versus mode

---
+++ Project-CandleLIT/Assets/SelectButtonOnEnable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectButtonOnEnable : MonoBehaviour {
+
+    public Button button;
+
+	private void OnEnable()
+    {
+        button.Select();
+        button.OnSelect(null);
+    }
+}
