ef56a8e66e3e59cc47b290824b1663d5e843d988
kmikus <kevinpmikus@gmail.com>
2018-04-25 13:21:01 -0400

    also fixed the volume slider on select and pause menu on select issues

---
+++ Project-CandleLIT/Assets/SelectSliderOnEnable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectSliderOnEnable : MonoBehaviour {
+
+    public Slider slider;
+	
+	private void OnEnable()
+    {
+        slider.Select();
+        slider.OnSelect(null);
+    }
+}
